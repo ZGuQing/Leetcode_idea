01e18709af739396b281d944082291b086bf252e
zgq <1595439942@qq.com>
2021-02-09 15:25:54 +0800

    leetcode 209:双指针求长度最小子数组

---
+++ src/lc_209/Solution_bip.java
@@ -0,0 +1,37 @@
+package lc_209;
+
+/**
+ * @author: Miguel
+ * @date: 2021/2/9 2:54 下午
+ * @description:
+ */
+public class Solution_bip {
+    /**
+     * 求长度最小子数组
+     * @param target
+     * @param nums
+     * @return
+     */
+    public int minSubArrayLen(int target, int[] nums) {
+        int n = nums.length;
+        int left = 0, right = 0;
+        int sum = 0, sumN = 0;
+        int res = Integer.MAX_VALUE;
+        for(int i = 0; i < n; i ++ ) {
+            sumN += nums[i];
+        }
+        if(sumN < target) return 0;
+        while(right < n) {
+            while(right < n && sum < target) {
+                sum += nums[right];
+                right ++;
+            }
+            while(left < n && sum >= target) {
+                sum -= nums[left];
+                left ++;
+            }
+            res = Math.min(res, right - left + 1);
+        }
+        return res;
+    }
+}
