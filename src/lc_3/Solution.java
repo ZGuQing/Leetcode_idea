b171f5a30cc21b0df3c779bd4fb91fc79637949e
zgq <1595439942@qq.com>
2021-02-10 17:00:07 +0800

    leetcode 3:双指针

--- src/lc_3/Solution.java
+++ src/lc_3/Solution.java
@@ -10,11 +10,13 @@ public class Solution {
     public int lengthOfLongestSubstring(String s) {
         //双指针算法
         int res = 0;
-        int[] count = new int[10000];
-        for (int i = 0, j = 0; i < s.length(); i++) {
-            count[s.charAt(i)] ++;
-            while(count[s.charAt(i)] > 1){//判断当前j～i有无重复字母
-                count[s.charAt(j)] --;
+        char[] chars = s.toCharArray();
+        int []count = new int[200];
+        //i右边界，j左边界
+        for(int i = 0, j = 0; i < s.length(); i ++){
+            count[chars[i]] ++;
+            while(count[chars[i]] > 1){
+                count[chars[j]] --;
                 j ++;
             }
             res = Math.max(res, i - j + 1);
